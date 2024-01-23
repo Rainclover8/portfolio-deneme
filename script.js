c7816ee3f692ac281ccc0d3cac3b1448a289d382
merahab <ts.baran6161@gmail.com>
2024-01-23 19:41:37 +0300

    form

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ window.addEventListener('scroll', () =>{
         home.classList.add('active')
         about.classList.remove('active')
         contact.classList.remove('active')
-    }else if(scrollY > 718 && scrollY < 800){
+    }else if(scrollY > 718 && scrollY < 900){
         about.classList.add('active')
         home.classList.remove('active')
         contact.classList.remove('active')
@@ -34,3 +34,35 @@ window.addEventListener('scroll', () =>{
         home.classList.remove('active')
     }
 })
+
+
+// let toplam = 0.1 + 0.2;
+// console.log(toplam );
+
+// let toplam2 = 0.2 + 0.3; 
+// console.log(toplam2 === 0.5);
+
+let deneme = document.getElementById('nameSurname').value
+let mail = document.getElementById('mail').value
+let tavsiye = document.getElementById('tavsiye').value
+
+let gosterMsg = "Adı Soyadı:" + deneme + "Mail:" + mail + "tavsiye metni: "+ tavsiye + "."
+// `
+// Ad: ${name}
+// mail: ${mail}
+// tavsiye metni: ${tavsiye}
+// `
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "barancicek.dev@gmail.com",
+        Password : "25AB6EB503A224EEB635C6B893B21B56E63A",
+        To : "ts.baran6161@gmail.com",
+        From : "barancicek.dev@gmail.com",
+        Subject : "New Contact Form Enquiry",
+        Body : gosterMsg,
+    }).then(
+      message => alert(message)
+    );
+    
+}
